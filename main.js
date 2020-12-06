0ba6c580dcc5f691e9eacecd18309d5bb4747c00
Marsel <marsel.kadroff@gmail.com>
2020-12-07 00:58:15 +0400

    fixed bugs

--- main.js
+++ main.js
@@ -12,6 +12,29 @@ let electro = 0;
 let wage = 0;
 let crash = 0;
 
+let potatoesOne = 0;
+let potatoesTwo = 0;
+let potatoesThree = 0;
+
+let carrotOne = 0;
+let carrotTwo = 0;
+let carrotThree = 0;
+
+let pepperOne = 0;
+let pepperTwo = 0;
+let pepperThree = 0;
+
+let tomatoOne = 0;
+let tomatoTwo = 0;
+let tomatoThree = 0;
+
+let cabbageOne = 0;
+let cabbageTwo = 0;
+let cabbageThree = 0;
+
+let parsleyOne = 0;
+let parsleyTwo = 0;
+let parsleyThree = 0;
 
 let hrs = 0
 let mins = 0
@@ -54,56 +77,55 @@ setInterval(() => {
         electro += 20
         wage += 30
 
-        if (potatoesSum > 80) {
+        if (potatoesOne > 0 && potatoesTwo > 0 && potatoesThree > 0) {
             alert("Контейнеры картофеля заполнены, они отправлены на загрузку")
             potatoes = [0, 0, 0];
-        } else if (carrotSum > 80) {
+        } else if (carrotOne > 0 && carrotTwo > 0 && carrotThree > 0) {
             alert("Контейнеры с морковью заполнены, они отправлены на загрузку")
             carrot = [0, 0, 0];
-        } else if (pepperSum > 80) {
+        } else if (pepperOne > 0 && pepperTwo > 0 && pepperThree > 0) {
             alert("Контейнеры с перцами заполнены, они отправлены на загрузку")
             pepper = [0, 0, 0];
-        } else if (tomatoSum > 80) {
+        } else if (tomatoOne > 0 && tomatoTwo > 0 && tomatoThree > 0) {
             alert("Контейнеры с томатами заполнены, они отправлены на загрузку")
             tomato = [0, 0, 0];
-        } else if (cabbageSum > 80) {
+        } else if (cabbageOne > 0 && cabbageTwo > 0 && cabbageThree > 0) {
             alert("Контейнеры с капустой заполнены, они отправлены на загрузку")
             cabbage = [0, 0, 0];
-        } else if (parsleySum > 80) {
+        } else if (parsleyOne > 0 && parsleyTwo > 0 && parsleyThree > 0) {
             alert("Контейнеры с петрушкой заполнены, они отправлены на загрузку")
             parsley = [0, 0, 0];
         }
 
-
         // Картофель
-        document.getElementById('small_potatoes').innerHTML = "Мелкий " + '<strong>' + potatoes[0] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('big_potatoes').innerHTML = "Крупный " + '<strong>' + potatoes[1] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('refuse_potatoes').innerHTML = "Брак " + '<strong>' + potatoes[2] + '</strong>' + ' (тара по 15кг)';
+        document.getElementById('small_potatoes').innerHTML = `Мелкий <strong>  ${potatoes[0]} </strong>  (тара : ${potatoes[0] % 20 === 0 ? potatoesOne = potatoes[0].toString().slice(0, 1) : potatoesOne})`;
+        document.getElementById('big_potatoes').innerHTML = `Крупный<strong>  ${potatoes[1]} </strong>(тара: ${potatoes[1] % 15 === 0 ? potatoesTwo = potatoes[1].toString().slice(0, 1) : potatoesTwo})`;
+        document.getElementById('refuse_potatoes').innerHTML = `Брак <strong> ${potatoes[2]} </strong> (тара: ${potatoes[2] % 10 === 0 ? potatoesThree = potatoes[2].toString().slice(0, 1) : potatoesThree})`;
 
         // Морковь 
-        document.getElementById('carrot_small').innerHTML = "Мелкий " + '<strong>' + carrot[0] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('carrot_big').innerHTML = "Крупный " + '<strong>' + carrot[1] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('carrot_refuse').innerHTML = "Брак " + '<strong>' + carrot[2] + '</strong>' + ' (тара по 15кг)';
+        document.getElementById('carrot_small').innerHTML = `Мелкий <strong>  ${carrot[0]} </strong> (тара: ${carrot[0] % 20 === 0 ? carrotOne = carrot[0].toString().slice(0, 1) : carrotOne})`;
+        document.getElementById('carrot_big').innerHTML = `Крупный <strong> ${carrot[1]} </strong> (тара: ${carrot[1] % 15 === 0 ? carrotTwo = carrot[1].toString().slice(0, 1) : carrotTwo})`;
+        document.getElementById('carrot_refuse').innerHTML = `Брак <strong> ${carrot[2]} </strong> (тара: ${carrot[2] % 15 === 0 ? carrotThree = carrot[2].toString().slice(0, 1) : carrotThree})`;
 
         // Перец
-        document.getElementById('black_pepper').innerHTML = "Черный перец " + '<strong>' + pepper[0] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('red_pepper').innerHTML = "Красный перец " + '<strong>' + pepper[1] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('red_sweety_pepper').innerHTML = "Сладкий красный перец " + '<strong>' + pepper[2] + '</strong>' + ' (тара по 25кг)';
+        document.getElementById('black_pepper').innerHTML = `Черный перец <strong> ${pepper[0]} </strong> (тара: ${pepper[0] % 20 === 0 ? pepperOne = pepper[0].toString().slice(0, 1) : pepperOne})`;
+        document.getElementById('red_pepper').innerHTML = `Красный перец <strong> ${pepper[1]} </strong> (тара: ${pepper[1] % 15 === 0 ? pepperTwo = pepper[1].toString().slice(0, 1) : pepperTwo})`;
+        document.getElementById('red_sweety_pepper').innerHTML = `Сладкий красный перец <strong> ${pepper[2]} </strong> (тара: ${pepper[2] % 15 === 0 ? pepperThree = pepper[2].toString().slice(0, 1) : pepperThree})`;
 
         // Томаты
-        document.getElementById('medium_tomato').innerHTML = "Среднеранние томаты " + '<strong>' + tomato[0] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('easy_tomato').innerHTML = "Раннеспелые томаты " + '<strong>' + tomato[1] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('cherry_tomato').innerHTML = "Черри томаты " + '<strong>' + tomato[2] + '</strong>' + ' (тара по 25кг)';
+        document.getElementById('medium_tomato').innerHTML = `Среднеранние томаты <strong> ${tomato[0]} </strong> (тара: ${tomato[0] % 20 === 0 ? tomatoOne = tomato[0].toString().slice(0, 1) : tomatoOne})`;
+        document.getElementById('easy_tomato').innerHTML = `Раннеспелые томаты <strong> ${tomato[1]} </strong> (тара: ${tomato[1] % 15 === 0 ? tomatoTwo = tomato[1].toString().slice(0, 1) : tomatoTwo})`;
+        document.getElementById('cherry_tomato').innerHTML = `Черри томаты <strong> ${tomato[2]} </strong> (тара: ${tomato[2] % 15 === 0 ? tomatoThree = tomato[2].toString().slice(0, 1) : tomatoThree})`;
 
         // Капуста
-        document.getElementById('white_cabbage').innerHTML = "Белокочанная капуста " + '<strong>' + cabbage[0] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('colors_cabbage').innerHTML = "Цветная капуста " + '<strong>' + cabbage[1] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('red_cabbage').innerHTML = "Краснокочанная капуста " + '<strong>' + cabbage[2] + '</strong>' + ' (тара по 25кг)';
+        document.getElementById('white_cabbage').innerHTML = `Белокочанная капуста <strong> ${cabbage[0]} </strong> (тара: ${cabbage[0] % 20 === 0 ? cabbageOne = cabbage[0].toString().slice(0, 1) : cabbageOne})`;
+        document.getElementById('colors_cabbage').innerHTML = `Цветная капуста <strong> ${cabbage[1]} </strong>  (тара: ${cabbage[1] % 15 === 0 ? cabbageTwo = cabbage[1].toString().slice(0, 1) : cabbageTwo})`;
+        document.getElementById('red_cabbage').innerHTML = `Краснокочанная капуста <strong> ${cabbage[2]} </strong>  (тара: ${cabbage[2] % 15 === 0 ? cabbageThree = cabbage[2].toString().slice(0, 1) : cabbageThree})`;
 
         // Петрушка
-        document.getElementById('list_parsley').innerHTML = "Листовая " + '<strong>' + parsley[0] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('curly_parsley').innerHTML = "Кудрявая " + '<strong>' + parsley[1] + '</strong>' + ' (тара по 25кг)';
-        document.getElementById('fail_parsley').innerHTML = "Брак " + '<strong>' + parsley[2] + '</strong>' + ' (тара по 15кг)';
+        document.getElementById('list_parsley').innerHTML = `Листовая <strong>  ${parsley[0]} </strong> (тара: ${parsley[0] % 20 === 0 ? parsleyOne = parsley[0].toString().slice(0, 1) : parsleyOne})`;
+        document.getElementById('curly_parsley').innerHTML = `Кудрявая <strong> ${parsley[1]} </strong> (тара: ${parsley[1] % 15 === 0 ? parsleyTwo = parsley[1].toString().slice(0, 1) : parsleyTwo})`;
+        document.getElementById('fail_parsley').innerHTML = `Брак <strong> ${parsley[2]} </strong> (тара: ${parsley[2] % 15 === 0 ? parsleyThree = parsley[2].toString().slice(0, 1) : parsleyThree})`;
 
 
         document.getElementById('vegetables').innerHTML = "Количество неотсортированных овощей: " + '<strong>' + vegetablesCount + '</strong>';
@@ -181,7 +203,7 @@ function startWatch() {
             mins = 0
         }
     }
-    document.querySelector('#session_time').innerHTML = `Время текущей сессии: ${hrs}:${mins}:${secs}`
+    document.querySelector('#session_time').innerHTML = `Время текущей сессии: ${hrs}: ${mins}: ${secs} `
 }
 
 
